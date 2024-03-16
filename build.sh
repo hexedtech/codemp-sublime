0062c292a875835d7321c5d86140edee5897573a
Camillo Schenone <camillo.schenone@gmail.com>
2024-03-16 15:51:49 +0100

    fix: cleaned up build script, version bumps
    Former-commit-id: 30e5f85a3e0bf9c4fbb3521b287043b6f55e76a6

--- build.sh
+++ build.sh
@@ -8,13 +8,11 @@ TARGET_DIR="$ROOT_DIR/bindings"
 TARGET_NAME="codemp_client"
 
 PYO3_PYTHON="$(pyenv which python)"
-PYTHON_SYS_EXECUTABLE="$PYO3_PYTHON"
 TARGET_EXT="$($PYO3_PYTHON -c 'import sysconfig; print(sysconfig.get_config_var("EXT_SUFFIX"))')"
 FULL_TARGET="${TARGET_NAME}${TARGET_EXT}"
 
 echo "Building with python: $PYO3_PYTHON"
-env PYO3_PYTHON="${PYO3_PYTHON}" PYTHON_SYS_EXECUTABLE="$PYO3_PYTHON" cargo build
+env PYO3_PYTHON="${PYO3_PYTHON}" cargo build
 echo "Copying into: $TARGET_DIR/$FULL_TARGET"
 
-[[ -f "$TARGET_DIR/$FUll_TARGET" ]] && echo "$FILE exists."
 ditto "$BUILD_DIR/${FILENAME}.dylib" "$TARGET_DIR/$FULL_TARGET"
