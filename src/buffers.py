822dc9a045a376162ad062c10fbc90cee69d9745
cschen <camillo.schenone@gmail.com>
2024-08-29 08:17:52 +0200

    fix: miscelaneous fixes
    Former-commit-id: f008552b7a5ca5f57a9802df6e7b1a09102afc74

--- src/buffers.py
+++ src/buffers.py
@@ -1,11 +1,51 @@
+from __future__ import annotations
+
 import sublime
 import os
 import logging
 
 import codemp
 from Codemp.src import globals as g
+from Codemp.src.utils import populate_view
 
 logger = logging.getLogger(__name__)
+
+
+def make_bufferchange_cb(buff: VirtualBuffer):
+    def __callback(bufctl: codemp.BufferController):
+        def get_change_and_apply():
+            change_id = buff.view.change_id()
+            while change := bufctl.try_recv().wait():
+                logger.debug("received remote buffer change!")
+                if change is None:
+                    break
+
+                if change.is_empty():
+                    logger.debug("change is empty. skipping.")
+                    continue
+
+                # In case a change arrives to a background buffer, just apply it.
+                # We are not listening on it. Otherwise, interrupt the listening
+                # to avoid echoing back the change just received.
+                if buff.view.id() == g.ACTIVE_CODEMP_VIEW:
+                    buff.view.settings()[g.CODEMP_IGNORE_NEXT_TEXT_CHANGE] = True
+
+                # we need to go through a sublime text command, since the method,
+                # view.replace needs an edit token, that is obtained only when calling
+                # a textcommand associated with a view.
+                buff.view.run_command(
+                    "codemp_replace_text",
+                    {
+                        "start": change.start,
+                        "end": change.end,
+                        "content": change.content,
+                        "change_id": change_id,
+                    },  # pyright: ignore
+                )
+
+        sublime.set_timeout(get_change_and_apply)
+
+    return __callback
 
 
 # This class is used as an abstraction between the local buffers (sublime side) and the
@@ -25,31 +65,21 @@ class VirtualBuffer:
     def __hash__(self) -> int:
         return hash(self.id)
 
+    def sync(self):
+        promise = self.buffctl.content()
+
+        def defer_sync(promise):
+            content = promise.wait()
+            populate_view(self.view, content)
+
+        sublime.set_timeout_async(lambda: defer_sync(promise))
+
     def cleanup(self):
         self.uninstall()
         self.buffctl.stop()
 
-    def install(self, rootdir):
-        if self.installed:
-            return
-
-        self.tmpfile = os.path.join(rootdir, self.id)
-        open(self.tmpfile, "a").close()
-
-        self.view.set_scratch(True)
-        self.view.set_name(self.id)
-        self.view.retarget(self.tmpfile)
-
-        s = self.view.settings()
-        self.view.set_status(g.SUBLIME_STATUS_ID, "[Codemp]")
-        s[g.CODEMP_BUFFER_TAG] = True
-
-        self.__activate()
-
-        self.installed = True
-
     def uninstall(self):
-        if not self.installed:
+        if not getattr(self, "installed", False):
             return
 
         self.__deactivate()
@@ -62,9 +92,29 @@ class VirtualBuffer:
 
         self.installed = False
 
+    def install(self, rootdir):
+        if getattr(self, "installed", False):
+            return
+
+        self.tmpfile = os.path.join(rootdir, self.id)
+        open(self.tmpfile, "a").close()
+
+        # self.view.set_scratch(True)
+        self.view.set_name(self.id)
+        self.view.retarget(self.tmpfile)
+
+        s = self.view.settings()
+        self.view.set_status(g.SUBLIME_STATUS_ID, "[Codemp]")
+        s[g.CODEMP_BUFFER_TAG] = True
+
+        self.sync()
+        self.__activate()
+
+        self.installed = True
+
     def __activate(self):
         logger.info(f"registering a callback for buffer: {self.id}")
-        self.buffctl.callback(self.__apply_bufferchange_cb)
+        self.buffctl.callback(make_bufferchange_cb(self))
         self.isactive = True
 
     def __deactivate(self):
@@ -85,35 +135,3 @@ class VirtualBuffer:
 
             # we must block and wait the send request to make sure the change went through ok
             self.buffctl.send(region.begin(), region.end(), change.str).wait()
-
-    def __apply_bufferchange_cb(self, bufctl: codemp.BufferController):
-        def get_change_and_apply():
-            change_id = self.view.change_id()
-            while change := bufctl.try_recv().wait():
-                if change is None:
-                    break
-
-                if change.is_empty():
-                    logger.debug("change is empty. skipping.")
-                    continue
-
-                # In case a change arrives to a background buffer, just apply it.
-                # We are not listening on it. Otherwise, interrupt the listening
-                # to avoid echoing back the change just received.
-                if self.view.id() == g.ACTIVE_CODEMP_VIEW:
-                    self.view.settings()[g.CODEMP_IGNORE_NEXT_TEXT_CHANGE] = True
-
-                # we need to go through a sublime text command, since the method,
-                # view.replace needs an edit token, that is obtained only when calling
-                # a textcommand associated with a view.
-                self.view.run_command(
-                    "codemp_replace_text",
-                    {
-                        "start": change.start,
-                        "end": change.end,
-                        "content": change.content,
-                        "change_id": change_id,
-                    },  # pyright: ignore
-                )
-
-        sublime.set_timeout(get_change_and_apply)
