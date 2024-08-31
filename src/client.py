16e14361651b33b4add97aacb06617835093eb53
cschen <camillo.schenone@gmail.com>
2024-08-31 15:24:22 +0200

    fix: simplify simplify simplify
    Former-commit-id: 3e093fcaba0026c2585aca0ea9b714852f5e9839

--- src/client.py
+++ src/client.py
@@ -13,6 +13,7 @@ from Codemp.src.utils import bidict
 
 logger = logging.getLogger(__name__)
 
+
 # the client will be responsible to keep track of everything!
 # it will need 3 bidirectional dictionaries and 2 normal ones
 # normal: workspace_id -> VirtualWorkspaces
@@ -20,41 +21,34 @@ logger = logging.getLogger(__name__)
 # bidir: VirtualBuffer <-> VirtualWorkspace
 # bidir: VirtualBuffer <-> Sublime.View
 # bidir: VirtualWorkspace <-> Sublime.Window
+def log_async(msg):
+    sublime.set_timeout_async(lambda: logger.log(logger.level, msg))
 
 
 class VirtualClient:
     def __init__(self):
         self.codemp: Optional[codemp.Client] = None
-        self.driver = codemp.init(lambda msg: logger.log(logger.level, msg), False)
+        self.driver: Optional[codemp.Driver] = None
 
         # bookkeeping corner
         self._id2buffer: dict[str, VirtualBuffer] = {}
         self._id2workspace: dict[str, VirtualWorkspace] = {}
-        self._view2buff: dict[sublime.View, VirtualBuffer] = {}
 
+        self._view2buff: dict[sublime.View, VirtualBuffer] = {}
         self._buff2workspace: bidict[VirtualBuffer, VirtualWorkspace] = bidict()
-        self._workspace2window: bidict[VirtualWorkspace, sublime.Window] = bidict()
+        # self._workspace2window: bidict[VirtualWorkspace, sublime.Window] = bidict()
+        self._workspace2window: dict[VirtualWorkspace, sublime.Window] = bidict()
 
     def dump(self):
         logger.debug("CLIENT STATUS:")
         logger.debug("WORKSPACES:")
         logger.debug(f"{self._id2workspace}")
         logger.debug(f"{self._workspace2window}")
-        logger.debug(f"{self._workspace2window.inverse}")
         logger.debug(f"{self._buff2workspace}")
         logger.debug(f"{self._buff2workspace.inverse}")
         logger.debug("VIEWS")
         logger.debug(f"{self._view2buff}")
         logger.debug(f"{self._id2buffer}")
-
-    def valid_window(self, window: sublime.Window):
-        return window in self._workspace2window.inverse
-
-    def valid_workspace(self, workspace: VirtualWorkspace | str):
-        if isinstance(workspace, str):
-            return client._id2workspace.get(workspace) is not None
-
-        return workspace in self._workspace2window
 
     def all_workspaces(
         self, window: Optional[sublime.Window] = None
@@ -62,14 +56,18 @@ class VirtualClient:
         if window is None:
             return list(self._workspace2window.keys())
         else:
-            return self._workspace2window.inverse.get(window, [])
+            return [
+                ws
+                for ws in self._workspace2window
+                if self._workspace2window[ws] == window
+            ]
 
     def workspace_from_view(self, view: sublime.View) -> Optional[VirtualWorkspace]:
         buff = self._view2buff.get(view, None)
-        return self._buff2workspace.get(buff, None)
+        return self.workspace_from_buffer(buff) if buff is not None else None
 
-    def workspace_from_buffer(self, buff: VirtualBuffer) -> Optional[VirtualWorkspace]:
-        return self._buff2workspace.get(buff)
+    def workspace_from_buffer(self, vbuff: VirtualBuffer) -> Optional[VirtualWorkspace]:
+        return self._buff2workspace.get(vbuff, None)
 
     def workspace_from_id(self, id: str) -> Optional[VirtualWorkspace]:
         return self._id2workspace.get(id)
@@ -78,10 +76,11 @@ class VirtualClient:
         self, workspace: Optional[VirtualWorkspace | str] = None
     ) -> list[VirtualBuffer]:
         if workspace is None:
-            return list(self._buff2workspace.keys())
+            return list(self._id2buffer.values())
+        elif isinstance(workspace, str):
+            workspace = client._id2workspace[workspace]
+            return self._buff2workspace.inverse.get(workspace, [])
         else:
-            if isinstance(workspace, str):
-                workspace = client._id2workspace[workspace]
             return self._buff2workspace.inverse.get(workspace, [])
 
     def buffer_from_view(self, view: sublime.View) -> Optional[VirtualBuffer]:
@@ -98,18 +97,12 @@ class VirtualClient:
         self._id2buffer[buffer.id] = buffer
         self._view2buff[buffer.view] = buffer
 
-    def unregister_buffer(self, buffer: VirtualBuffer):
-        del self._buff2workspace[buffer]
-        del self._id2buffer[buffer.id]
-        del self._view2buff[buffer.view]
-
     def disconnect(self):
         if self.codemp is None:
             return
         logger.info("disconnecting from the current client")
         # for each workspace tell it to clean up after itself.
         for vws in self.all_workspaces():
-            vws.cleanup()
             self.codemp.leave_workspace(vws.id)
 
         self._id2workspace.clear()
@@ -124,35 +117,37 @@ class VirtualClient:
             logger.info("Disconnecting from previous client.")
             return self.disconnect()
 
-        self.codemp = codemp.Client(host, user, password)
+        if self.driver is None:
+            self.driver = codemp.init()
+            codemp.set_logger(log_async, False)
+
+        self.codemp = codemp.connect(host, user, password).wait()
         id = self.codemp.user_id()
         logger.debug(f"Connected to '{host}' as user {user} (id: {id})")
 
-    def install_workspace(
-        self, workspace: codemp.Workspace, window: sublime.Window
-    ) -> VirtualWorkspace:
-        # we pass the window as well so if the window changes in the mean
-        # time we have the correct one!
+    def install_workspace(self, workspace: codemp.Workspace, window: sublime.Window):
         vws = VirtualWorkspace(workspace, window)
         self._workspace2window[vws] = window
         self._id2workspace[vws.id] = vws
 
-        vws.install()
-        return vws
-
     def uninstall_workspace(self, vws: VirtualWorkspace):
-        if vws not in self._workspace2window:
-            raise
-
+        # we aim at dropping all references to the workspace
+        # as well as all the buffers associated with it.
+        # if we did a good job the dunder del method will kick
+        # and continue with the cleanup.
         logger.info(f"Uninstalling workspace '{vws.id}'...")
-        vws.cleanup()
         del self._workspace2window[vws]
         del self._id2workspace[vws.id]
-        buffers = self._buff2workspace.inverse[vws]
-        for vbuff in buffers:
+        for vbuff in self.all_buffers(vws):
             self.unregister_buffer(vbuff)
+        del vws
         # self._buff2workspace.inverse_del(vws) - if we delete all straight
         # keys the last delete will remove also the empty key.
+
+    def unregister_buffer(self, buffer: VirtualBuffer):
+        del self._buff2workspace[buffer]
+        del self._id2buffer[buffer.id]
+        del self._view2buff[buffer.view]
 
     def workspaces_in_server(self):
         return self.codemp.active_workspaces() if self.codemp else []
